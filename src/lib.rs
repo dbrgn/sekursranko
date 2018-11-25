f0b9bda0f0a5240d56937e46e52802fe9cad3249
Danilo Bargen <mail@dbrgn.ch>
2018-11-25 21:24:48 +0100

    Implement config endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,35 @@ use log::warn;
 
 static VERSION: &str = env!("CARGO_PKG_VERSION");
 
+/// The server configuration.
+/// TODO: Serialize
+struct ServerConfig {
+    max_backup_bytes: u32,
+    retention_days: u32,
+}
+
+// TODO: Make configurable
+static SERVER_CONFIG: ServerConfig = ServerConfig {
+    max_backup_bytes: 524288,
+    retention_days: 180,
+};
+
 type BoxFut = Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>;
+
+// "application/json"
+macro_rules! require_accept_starts_with {
+    ($req:expr, $resp:expr, $accept:expr) => {
+        match $req.headers().get(header::ACCEPT).and_then(|v| v.to_str().ok()) {
+            Some(accept) if accept.starts_with($accept) => {},
+            _ => {
+                warn!("Received request without valid accept header");
+                *$resp.status_mut() = StatusCode::BAD_REQUEST;
+                *$resp.body_mut() = Body::from("{\"detail\": \"Invalid accept header\"}");
+                return;
+            }
+        }
+    }
+}
 
 /// Main handler.
 pub fn handler(req: Request<Body>) -> BoxFut {
@@ -19,14 +47,6 @@ pub fn handler(req: Request<Body>) -> BoxFut {
     let mut response = Response::new(Body::empty());
 
     // Verify headers
-    match req.headers().get(header::ACCEPT).and_then(|v| v.to_str().ok()) {
-        Some(accept) if accept.starts_with("application/json") => {},
-        _ => {
-            warn!("Received request without valid accept header");
-            *response.status_mut() = StatusCode::BAD_REQUEST;
-            return Box::new(future::ok(response));
-        }
-    }
     match req.headers().get(header::USER_AGENT).and_then(|v| v.to_str().ok()) {
         Some(uagent) if uagent.contains("Threema") => {},
         _ => {
@@ -38,6 +58,7 @@ pub fn handler(req: Request<Body>) -> BoxFut {
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => handle_index(&mut response),
+        (&Method::GET, "/config") => handle_config(&req, &mut response),
         _ => handle_404(&mut response),
     }
     Box::new(future::ok(response))
@@ -47,7 +68,17 @@ fn handle_index(response: &mut Response<Body>) {
     *response.body_mut() = Body::from(format!("rustysafe {}", VERSION));
 }
 
+fn handle_config(request: &Request<Body>, response: &mut Response<Body>) {
+    require_accept_starts_with!(request, response, "application/json");
+
+    *response.status_mut() = StatusCode::OK;
+    *response.body_mut() = Body::from(format!(
+        "{{\"maxBackupBytes\": {}, \"retentionDays\": {}}}",
+        SERVER_CONFIG.max_backup_bytes,
+        SERVER_CONFIG.retention_days,
+    ));
+}
+
 fn handle_404(response: &mut Response<Body>) {
     *response.status_mut() = StatusCode::NOT_FOUND;
 }
-
--- tests/integration.rs
+++ tests/integration.rs
@@ -21,40 +21,66 @@ fn testserver() -> (thread::JoinHandle<()>, String) {
     (handle, format!("http://127.0.0.1:{}", port))
 }
 
+macro_rules! user_agent_required {
+    ($name:ident, $url:expr) => {
+        #[test]
+        fn $name() {
+            let (_handle, base_url) = testserver();
+            let client = Client::new();
+            let res = client
+                .get(&format!("{}{}", base_url, $url))
+                .send()
+                .unwrap();
+            assert_eq!(res.status().as_u16(), 400);
+        }
+    }
+}
+
+user_agent_required!(user_agent_required_index, "/");
+user_agent_required!(user_agent_required_config, "/config");
+
 #[test]
-fn headers_required() {
+fn index_ok() {
     let (_handle, base_url) = testserver();
     let client = Client::new();
-    // No headers
-    let res = client
+    let mut res = client
         .get(&base_url)
+        .header(header::USER_AGENT, "Foo Threema Bar")
         .send()
         .unwrap();
-    assert_eq!(res.status().as_u16(), 400);
+    let text = res.text().unwrap();
+    println!("{}", text);
+    assert_eq!(res.status().as_u16(), 200);
+    assert_eq!(text, "rustysafe 0.1.0");
 }
 
 #[test]
-fn accept_json_required() {
+fn config_require_json() {
     let (_handle, base_url) = testserver();
     let client = Client::new();
-    // Only user agent, no accept header
-    let res = client
-        .get(&base_url)
-        .header(header::USER_AGENT, "Threema")
+    let mut res = client
+        .get(&format!("{}/config", base_url))
+        .header(header::USER_AGENT, "Foo Threema Bar")
         .send()
         .unwrap();
+    let text = res.text().unwrap();
+    println!("{}", text);
     assert_eq!(res.status().as_u16(), 400);
+    assert_eq!(text, "{\"detail\": \"Invalid accept header\"}");
 }
 
 #[test]
-fn user_agent_required() {
+fn config_ok() {
     let (_handle, base_url) = testserver();
     let client = Client::new();
-    // Only accept header, no user agent
-    let res = client
-        .get(&base_url)
+    let mut res = client
+        .get(&format!("{}/config", base_url))
+        .header(header::USER_AGENT, "Foo Threema Bar")
         .header(header::ACCEPT, "application/json")
         .send()
         .unwrap();
-    assert_eq!(res.status().as_u16(), 400);
+    let text = res.text().unwrap();
+    println!("{}", text);
+    assert_eq!(res.status().as_u16(), 200);
+    assert_eq!(text, "{\"maxBackupBytes\": 524288, \"retentionDays\": 180}");
 }
