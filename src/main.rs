c668b0a3d0eabb6a44ede72929f4e640f7c6bed6
Danilo Bargen <mail@dbrgn.ch>
2022-06-26 16:44:55 +0200

    Upgrade to Clap 3

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,28 @@
-use std::path::Path;
+use std::path::PathBuf;
 
-use clap::{App, Arg};
+use clap::{self, Parser};
 use hyper::Server;
 use log::error;
 
 use sekursranko::{MakeBackupService, ServerConfig};
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    /// Path to the config file
+    #[clap(short, long)]
+    config: PathBuf,
+}
+
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() {
     env_logger::init();
 
-    let matches = App::new(sekursranko::NAME)
-        .version(sekursranko::VERSION)
-        .author("Danilo Bargen <mail@dbrgn.ch>")
-        .about("An efficient and memory-safe Threema Safe server implementation written in Rust.")
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("FILE")
-                .help("Path to the config file")
-                .takes_value(true)
-                .required(true),
-        )
-        .get_matches();
+    // Parse CLI args
+    let args = Args::parse();
 
     // Load config
-    let config_path_str = matches
-        .value_of("config")
-        .expect("Could not find config argument");
-    let config_path = Path::new(config_path_str);
-    let config: ServerConfig = ServerConfig::from_file(config_path).unwrap_or_else(|e| {
+    let config: ServerConfig = ServerConfig::from_file(&args.config).unwrap_or_else(|e| {
         eprintln!("Could not load config file: {}", e);
         ::std::process::exit(1);
     });
